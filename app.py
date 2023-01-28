898324ae787117487442d50cd7806f43d424386a
Akhil chandran <97656070+Cooperator-gem@users.noreply.github.com>
2023-01-28 08:45:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Cooperator-gem:token@github.com/Cooperator-gem/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Cooperator-gem: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@github.com/Cooperator-gem/forward ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
